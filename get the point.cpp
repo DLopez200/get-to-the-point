5e15094c4f49cecb41a53557b4097af37e12a635
DLopez200 <111823254+DLopez200@users.noreply.github.com>
2026-09-15 10:41:43 -0500

    start
    the start of the code

---
+++ get the point.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cmath>
+
+using namesapce std;
+
+class point{
+    private:
+    double x;
+    double y;
+
+    public:
+    double x = x;
+    double y =  y;
+
+    point& operator-(double x & other){
+        sqrt(())
+    }
+
+    int get_x(){
+        return x;
+    }
+    int get_y(){
+        return y;
+    }
+
+}
+
+int main(){
+    point p1 = point(3.2, 9.8);
+    point p2 = point(5.5, -1.2)
+
+    p1.get_x();
+    p2.get_x();
+
+    return 0;
+}
